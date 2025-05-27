6342ddad0a41b91dfef0e2444fc54ad7280ec346
Kamil Paduszyński <paduszyk@gmail.com>
2025-05-27 22:42:10 +0200

    Revert "Discard PaDEL featurization"
    This reverts commit 16c611080537a069d23a57aec4418789b02b7864.

--- src/ilml/featurization/featurizers.py
+++ src/ilml/featurization/featurizers.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from collections.abc import Callable
 from typing import TYPE_CHECKING, Any
 
+import padelpy
 from rdkit.Chem.Descriptors import CalcMolDescriptors
 
 from ilml.memory import cache
@@ -14,6 +15,8 @@ if TYPE_CHECKING:
     from ilml.chemistry import Ion, IonicLiquid
 
 rdkit_calc_mol_descriptors = cache(CalcMolDescriptors)
+
+padelpy_from_smiles = cache(padelpy.from_smiles)
 
 ion_featurizers: dict[str, IonFeaturizer] = {}
 
@@ -48,6 +51,12 @@ class IonFeaturizer(ABC):
 class RDKitIonFeaturizer(IonFeaturizer):
     def _featurize(self, ion: Ion) -> dict[str, Any]:
         return rdkit_calc_mol_descriptors(ion.rdkit_mol)  # type: ignore[no-any-return]
+
+
+@register("PaDEL")
+class PaDELIonFeaturizer(IonFeaturizer):
+    def _featurize(self, ion: Ion) -> dict[str, Any]:
+        return padelpy_from_smiles(ion.smiles)  # type: ignore[no-any-return]
 
 
 class IonicLiquidFeaturizer:
